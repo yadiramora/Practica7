9a074c5436299b5eed12e5a856579ca00130b5ba
yadiramora <almn.yadiracobos@cbtis66.edu.mx>
2022-10-12 19:54:17 -0500

    Practica7

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Practica7';
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string="";
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+
+  }
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }
+}
